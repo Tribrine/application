e699c09b4a11eba39f720478336815c72e2e4f70
KristianKirchev <47897192+KristianKirchev@users.noreply.github.com>
2024-09-10 17:31:02 +0300

    Injection (#11)
    * constructor injection
    
    * setter injection
    
    * field injection

--- src/main/java/com/tribrine/practice/di/MessageSender.java
+++ src/main/java/com/tribrine/practice/di/MessageSender.java
@@ -7,14 +7,60 @@ import org.springframework.stereotype.Component;
 @Component("msgSender")
 public class MessageSender {
 
+    @Autowired
+    @Qualifier("emailService")
     private final MessageService messageService;
 
     @Autowired
-    public MessageSender(@Qualifier("emailService") MessageService messageService) {
+    @Qualifier("sms")
+    private MessageService smsService;
+
+
+    ///////////////////////////////////////////////////////////////////////
+
+
+//    //@Autowired
+//    public MessageSender(@Qualifier("emailService") MessageService messageService) {
+//        this.messageService = messageService;
+//        System.out.println("Initializing MessageSender 1");
+//    }
+//
+//    @Autowired
+//    public MessageSender(@Qualifier("emailService") MessageService messageService, @Qualifier("sms") MessageService smsService) {
+//        this.messageService = messageService;
+//        this.smsService = smsService;
+//        System.out.println("Initializing MessageSender 2");
+//    }
+
+
+    ///////////////////////////////////////////////////////////////////////
+
+
+//    @Autowired
+//    public void setMessageService(@Qualifier("emailService") MessageService messageService) {
+//        this.messageService = messageService;
+//        System.out.println("Set MessageService MAIL");
+//    }
+//
+//    @Autowired
+//    public void setSmsService(@Qualifier("sms") MessageService smsService) {
+//        this.smsService = smsService;
+//        System.out.println("Set MessageService SMS");
+//    }
+
+
+    /////////////////////////////////////////////////////////////////////////
+
+
+    public MessageSender(MessageService messageService, MessageService smsService) {
         this.messageService = messageService;
+        this.smsService = smsService;
+        System.out.println("Field injection - final");
     }
 
     public void sendMessage(String message) {
+
         messageService.sendMessage(message);
+        smsService.sendMessage(message);
     }
 }
