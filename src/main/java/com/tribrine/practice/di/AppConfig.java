b651451566b731047c5f9a0113776408a45fcf2f
Kristian Kirchev <kris.kirchev@gmail.com>
2024-09-11 18:13:26 +0300

    migration

--- src/main/java/com/tribrine/practice/di/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.tribrine.practice.di;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.tribrine.practice.di")
-public class AppConfig {
-}
--- src/main/java/com/tribrine/practice/di/MessageService.java
+++
@@ -1,5 +0,0 @@
-package com.tribrine.practice.di;
-
-public interface MessageService {
-    void sendMessage(String message);
-}
